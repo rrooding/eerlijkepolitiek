b62bca972efc9f341d26b0c4183e019a246a5439
Ralph Rooding <ralph@izerion.com>
2012-09-03 20:43:51 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eerlijkepolitiek::Application.config.secret_token = '57a3a7955e9002623285bbdfdeb155d053d5f4a433dda44960e1a13b27691629e0273e2dc18edf05a560c3e39e911add3be6e20c60cb0bd0f3e6b63b4c5723c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Eerlijkepolitiek::Application.config.session_store :cookie_store, key: '_eerlijkepolitiek_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Eerlijkepolitiek::Application.config.session_store :active_record_store
